98fe9b3626b63eba108619f94de07c843bfd15de
Andy Ng <nga15@mills.cas.mcmaster.ca>
2018-01-30 10:50:06 -0500

    Added Bash01.hs

--- Labs/Bash01/Bash01.hs
+++ Labs/Bash01/Bash01.hs
@@ -1,5 +1,5 @@
 macid :: String
-macid = "nga15"
+macid = "yourmacid"
 
 answers :: [(Int,String)]
 answers = [(1,"terminus"), (2,"StoneChamber"), (3,"sudo"), (4,"Farm"), (5,"KernelFiles"), (6,"IHTFP")]
