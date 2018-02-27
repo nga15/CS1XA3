365fadb95e1b606b0087648bb09b058dafc2c7b3
Andy Ng <nga15@mills.cas.mcmaster.ca>
2018-02-26 21:30:50 -0500

    Completed all required functions

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Welcome to Andy's repo!"
+echo "(^o^)/ Welcome to Andy's repo! \(^o^)"
 echo "What would you like to do?"
-echo "Please enter a number to select your choice. Press 0 at any time to exit."
+echo "Please enter a number listed next to each option to select your choice."
 echo "1- Repo Status"
 echo "2- Update Uncommitted Changes"
-echo "3- Update TODO list"
-echo "4- Error check for Haskell Files"
-echo "5- Description of each feature"
+echo "3- Update TODO List"
+echo "4- Error Check for Haskell Files"
+echo "5- Description of Each Feature"
 echo "6- Run All"
 echo "0- Exit"
 
@@ -43,7 +43,7 @@ then
 	find ~/CS1XA3/ -name "*.hs" -print0 |
         	while IFS='' read -r -d $'\0' file
         	do
-                	ghc -fno-code "$file" >> error.log
+                	ghc -fno-code "$file" &>> error.log
         	done
 	echo "Errors have been checked for all Haskell Files and placed into error.log."
 	echo "Would you like to view the log? (Type Y/N)"
@@ -55,10 +55,52 @@ then
 	./ProjectAnalyze.sh
 elif [ $command -eq 5 ]
 then
-	echo "dX"
+	vim ~/CS1XA3/Assign1/README.txt
+	./ProjectAnalyze.sh
 elif [ $command -eq 6 ]
 then
-	echo "do everything here"
+	git status
+        
+	git status > changes.log
+        echo "Uncommitted Changes have been updated to changes.log."
+        echo "Would you like to view the log? (Type Y/N)"
+        read choice
+        if [ $choice = "Y" ]
+        then
+                vim changes.log
+        fi
+        
+	grep --exclude="todo.log" -r "#TODO" ~/CS1XA3/ > todo.log
+        echo "TODO List has been updated!"
+        echo "Would you like to view the log? (Type Y/N)"
+        read choice
+        if [ $choice = "Y" ]
+        then
+                vim todo.log
+        fi
+        
+	rm ~/CS1XA3/error.log
+        find ~/CS1XA3/ -name "*.hs" -print0 |
+                while IFS='' read -r -d $'\0' file
+                do
+                        ghc -fno-code "$file" &>> error.log
+                done
+        echo "Errors have been checked for all Haskell Files and placed into error.log."
+        echo "Would you like to view the log? (Type Y/N)"
+        read choice
+        if [ $choice = "Y" ]
+        then
+                vim error.log
+        fi
+        
+	echo "Would you like to view the Guide? (Type Y/N"
+	read choice
+	if [ $choice = "Y" ]
+	then
+		vim ~/CS1XA3/Assign1/README.txt
+	fi
+
+	./ProjectAnalyze.sh
 elif [ $command -eq 0 ]
 then
 	echo "Thanks for visiting~!"
