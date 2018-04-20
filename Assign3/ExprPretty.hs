8bc86f220e024c0c7a9ad6c7d6a58b26fbe4f73e
Andy Ng <nga15@mills.cas.mcmaster.ca>
2018-04-20 19:28:48 -0400

    'Completed' creating datatypes and 'prettying' up format

---
+++ Assign3/ExprPretty.hs
@@ -0,0 +1,33 @@
+{-|
+Module      : ExprPretty
+Description : Takes Expr expressions and returns it in a Pretty format
+Copyright   : (c) Andy Ng @2018
+Licence     : WTFPL
+Maintainer  : nga15@mcmaster.ca
+Stability   : experimental
+Portability : POSIX
+-}
+
+module ExprPretty where
+
+import           ExprType
+
+-- parens Function - Wraps the expression in parenthesis
+
+parens :: String -> String
+parens ss = "(" ++ ss ++ ")"
+
+-- Formats the expressions with the mathematical operators/ functions used
+
+instance Show a => Show (Expr a) where
+  show (Add e1 e2)  = parens (show e1) ++ " !+ " ++ parens (show e2)
+  show (Sub e1 e2)  = parens (show e1) ++ " !- " ++ parens (show e2)
+  show (Mult e1 e2) = parens (show e1) ++ " !* " ++ parens (show e2)
+  show (Div e1 e2)  = parens (show e1) ++ " !/ " ++ parens (show e2)
+  show (Sin e)      = "Sin" ++ parens (show e)
+  show (Cos e)      = "Cos" ++ parens (show e)
+  show (Tan e)      = "Tan" ++ parens (show e)
+  show (Log n e)    = "Log" ++ show (n) ++ " " ++ parens (show e)
+  show (Exp e1 e2)  = parens (show e1) ++ "**" ++ parens (show e2)
+  show (Var x)      = x
+  show (Const x)    = show x
---
+++ Assign3/ExprType.hs
@@ -0,0 +1,43 @@
+{-|
+Module      : ExprType
+Description : ...
+Copyright   : (c) Andy Ng @2018
+Licence     : WTFPL
+Maintainer  : nga15@mcmaster.ca
+Stability   : experimental
+Portability : POSIX
+-}
+
+module ExprType where
+
+import           Data.List
+
+-- Declaration for the Expr Datatype
+
+data Expr a = Add (Expr a) (Expr a)     -- Addition
+            | Sub (Expr a) (Expr a)     -- Subtraction
+            | Mult (Expr a) (Expr a)    -- Mulitplication
+            | Div (Expr a) (Expr a)     -- Division
+            | Sin (Expr a)              -- Sine
+            | Cos (Expr a)              -- Cosine
+            | Tan (Expr a)              -- Tangent
+            | Log a (Expr a)            -- Log base a
+            | Exp (Expr a) (Expr a)     -- Exp - Expr a to the Expr a
+            | Const a                   -- Constant
+            | Var String                -- Variable
+  deriving Eq
+
+-- getVars Function - ... 
+
+getVars :: Expr a -> [String]
+getVars (Add e1 e2)  = getVars e1 ++ getVars e2
+getVars (Sub e1 e2)  = getVars e1 ++ getVars e2
+getVars (Mult e1 e2) = getVars e1 ++ getVars e2
+getVars (Div e1 e2)  = getVars e1 ++ getVars e2
+getVars (Sin e)      = getVars e
+getVars (Cos e)      = getVars e
+getVars (Tan e)      = getVars e
+getVars (Log n e)    = getVars e
+getVars (Exp e1 e2)  = getVars e1 ++ getVars e2
+getVars (Const _)    = []
+getVars (Var x)      = [x]
