6e8472b7a5e03364e2aa1b475eb8b65b2873d30e
Andy Ng <nga15@mills.cas.mcmaster.ca>
2018-04-20 23:56:46 -0400

    Added partial derivatives

--- Assign3/ExprDiff.hs
+++ Assign3/ExprDiff.hs
@@ -13,8 +13,12 @@ class DiffExpr a where
 
   (!+) :: Expr a -> Expr a -> Expr a
   e1 !+ e2 = simplify (Map.fromList []) $ Add e1 e2
+  (!-) :: Expr a -> Expr a -> Expr a
+  e1 !- e2 = simplify (Map.fromList []) $ Sub e1 e2
   (!*) :: Expr a -> Expr a -> Expr a
   e1 !* e2 = simplify (Map.fromList []) $ Mult e1 e2
+  (!/) :: Expr a -> Expr a -> Expr a
+  e1 !/ e2 = simplify (Map.fromList []) $ Div e1 e2
   val :: a -> Expr a
   val x = Const x
   var :: String -> Expr a
@@ -29,4 +33,46 @@ instance (Num a) => DiffExpr a where
                        Just v  -> v
                        Nothing -> error "failed lookup in eval"
   simplify _ e = e -- #TODO finish me!
-  partDiff _ e = e -- #TODO finish me!
+  --partDiff _ e = e -- #TODO finish me!
+
+  partDiff x (Add e1 e2) = Add (partDiff x e1) (partDiff x e2)
+  partDiff x (Sub e1 e2) = Sub (partDiff x e1) (partDiff x e2)
+  partDiff x (Mult e1 e2) = let
+                                p1 = Mult (partDiff x e1) e2
+                                p2 = Mult e1 (partDiff x e2)
+                            in Add p1 p2
+  partDiff x (Div e1 e2) = let 
+                                p1 = Mult (partDiff x e1) e2
+                                p2 = Mult e1 (partDiff x e2)
+                                p3 = Pow (e2) (Const (2))
+                            in Div (Sub p1 p2) p3
+
+  partDiff x (Sin e) = Mult (Cos (e)) (partDiff x e) 
+  partDiff x (Cos e) = Mult (Mult (Sin (e)) (partDiff x e)) (Const (-1))
+  partDiff x (Tan e) = let
+                            p1 = Div (Const 1) (Pow (Cos e) (Const(2)))
+                            p2 = partDiff x e
+                        in Mult p1 p2
+
+  partDiff x (Log n e) = let 
+                            p1 = Div (Const (1)) (Ln (Const (n)))
+                            p2 = Div (Const (1)) e
+                            p3 = partDiff x e
+                        in Mult (Mult p1 p2) p3
+  partDiff x (Ln e) = let
+                            p1 = Div (Const (1)) e
+                            p2 = partDiff x e
+                        in Mult p1 p2
+
+  partDiff x (Exp n e) = let
+                            p1 = Exp n e
+                            p2 = Ln (Const n)
+                            p3 = partDiff x e
+                        in Mult (Mult p1 p2) p3
+  partDiff x (Pow e1 e2) = let
+                            p1 = (Const (2)) 
+                            p2 = (Mult e2 (Ln (e1)))
+                        in Mult p1 p2
+
+  partDiff _ (Const a) = Const 0
+  partDiff x (Var a) = if a == x then Const (1) else Const (0)
--- Assign3/ExprType.hs
+++ Assign3/ExprType.hs
@@ -22,7 +22,9 @@ data Expr a = Add (Expr a) (Expr a)     -- Addition
             | Cos (Expr a)              -- Cosine
             | Tan (Expr a)              -- Tangent
             | Log a (Expr a)            -- Log base a
-            | Exp (Expr a) (Expr a)     -- Exp - Expr a to the Expr a
+            | Ln (Expr a)               -- Ln
+            | Exp a (Expr a)            -- Exp - Const n to the Expr a
+            | Pow (Expr a) (Expr a)     -- Power - Expr a to the n
             | Const a                   -- Constant
             | Var String                -- Variable
   deriving Eq
@@ -38,6 +40,8 @@ getVars (Sin e)      = getVars e
 getVars (Cos e)      = getVars e
 getVars (Tan e)      = getVars e
 getVars (Log n e)    = getVars e
-getVars (Exp e1 e2)  = getVars e1 ++ getVars e2
+getVars (Ln e)       = getVars e
+getVars (Exp n e)    = getVars e
+getVars (Pow e1 e2)  = getVars e1 ++ getVars e2
 getVars (Const _)    = []
 getVars (Var x)      = [x]
